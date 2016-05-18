0c8b1aa45631c78a49080636dc8b2a00c521b003
2k0ri <esc13245@gmail.com>
2016-05-18 20:55:46 +0900

    add sbt dependencies

--- build.sbt
+++ build.sbt
@@ -8,5 +8,8 @@ libraryDependencies ++= Seq(
   "org.apache.spark" % "spark-core_2.10" % "1.6.0",
   "org.apache.spark" % "spark-sql_2.10" % "1.6.0",
   "com.databricks" % "spark-avro_2.10" % "2.0.1",
-  "mysql" % "mysql-connector-java" % "6.0.2"
+  // "com.databricks" % "spark-csv_2.10" % "1.4.0",
+  "mysql" % "mysql-connector-java" % "6.0.2", // MySQL
+  "com.microsoft.sqlserver" % "sqljdbc42" % "4.2" // SQL Server (not in central repo)
+  // "net.starschema.clouddb.bqjdbc" % "bqjdbc" % "1.4" // @TODO BigQuery JDBC (not in central repo) @see https://code.google.com/archive/p/starschema-bigquery-jdbc/wikis/JDBCURL.wiki
 )
--- src/main/scala/com/aucfanlab/datalake/SparkJDBCExportJob.scala
+++ src/main/scala/com/aucfanlab/datalake/SparkJDBCExportJob.scala
@@ -9,29 +9,29 @@ import org.apache.spark.{SparkConf, SparkContext}
 /**
   * get Avro file and insert to RDB through JDBC connector.
   * Usage:
-  *   SparkJDBCExportJob.jar avroFile jdbcString jdbcTableName -Dcom.aucfalab.datalake.arraycolumns="columntojoin,columntojoin2"
+  * SparkJDBCExportJob.jar avroFile jdbcString jdbcTableName -Dcom.aucfalab.datalake.arraycolumns="columntojoin,columntojoin2"
   */
 object SparkJDBCExportJob {
   def main(args: Array[String]) {
-    val avroFile = args(0)
+    val file = args(0)
     val jdbcString = args(1)
     val jdbcTableName = args(2)
 
-    val arrayColumns = Option(System.getProperty("com.aucfanlab.datalake.arraycolumns")).getOrElse("").split(",")
+    val arrayColumns = Option(System.getProperty("com.aucfanlab.datalake.arraycolumns"))
 
     val conf = new SparkConf().setAppName(s"SparkJDBCExportJob_$jdbcTableName")
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
-
     // val avroFile = "/usr/local/opt/apache-spark/libexec/examples/src/main/resources/users.avro"
-    var df = sqlContext.read.avro(avroFile)
-    // df.show()
+    var df = sqlContext.read.avro(file)
+    // var df = sqlContext.read.json(file)
 
     // join array as string
     val mkString = org.apache.spark.sql.functions.udf((a: Seq[Object]) => a.mkString(","))
-    for (c <- arrayColumns) {
-      df = df.withColumn(c, mkString(df(c)))
-      // df.show()
+    if (arrayColumns.isDefined) {
+      for (c <- arrayColumns.getOrElse("").split(",")) {
+        if (!c.isEmpty) df = df.withColumn(c, mkString(df(c)))
+      }
     }
 
     // val jdbcString = "jdbc:mysql://localhost:3306/hadooptest?user=root&useSSL=false"
